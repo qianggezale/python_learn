5675c06c4118bb69fd7458ee307db0b676b64342
qianggezale <lqrj1012@sina.com>
2018-05-30 10:49:48 +0800

    c

--- hello.py
+++ hello.py
@@ -4,8 +4,36 @@
 # n2=input('请输入第二个数字')
 # print('之和为：',n1+n2)
 
-a = 100
-if (a > 1):
-    print(1)
-else:
-    print(11)
+# a = 100
+# if (a > 1):
+#     print(1)
+# else:
+#     print(11)
+
+# print('I\'m \nok.')
+
+# a='ABC'
+# b=a
+# a='XYZ'
+# print(b)
+
+# print(10/3)
+# print(9/3)
+# print(10//3)
+# import math
+# print(math.pi)
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+s5=r'''ss"ss"dfsafd
+ssssdfsafd
+'''
+print(s3)
+print(s4)
+print(s5)
+
