5977b9c34d0e86fde1775e9c14ba6cbe1de575d4
qianggezale <lqrj1012@sina.com>
2018-05-30 14:36:31 +0800

    字符串

--- hello.py
+++ hello.py
@@ -23,17 +23,34 @@
 # import math
 # print(math.pi)
 
-n = 123
-f = 456.789
-s1 = 'Hello, world'
-s2 = 'Hello, \'Adam\''
-s3 = r'Hello, "Bart"'
-s4 = r'''Hello,
-Lisa!'''
-s5=r'''ss"ss"dfsafd
-ssssdfsafd
-'''
-print(s3)
-print(s4)
-print(s5)
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa!'''
+# s5=r'''ss"ss"dfsafd
+# ssssdfsafd
+# '''
+# print(s3)
+# print(s4)
+# print(s5)
+
+
+# # 对于单个字符的编码，Python提供了ord()函数获取字符的整数表示，chr()函数把编码转换为对应的字符
+# print(ord('S'))
+# print(ord('强'))
+# print(chr(66))
+
+# # s
+# print('ABC'.encode('ascii'))
+# print('强哥'.encode('utf-8'))
+
+# print(b'\xe5\xbc\xba\xe5\x93\xa5'.decode('utf-8'))
+# len()函数计算的是str的字符数，如果换成bytes，len()函数就计算字节数：
+print(len('qiang'))
+print(len('qiang1'.encode('ascii')))
+print(len('强'.encode('utf-8')))
+
 
