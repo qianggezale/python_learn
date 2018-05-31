81ff9b12d6a0a8b0b1f23f3ec124b570d2ae3bf8
qianggezale <lqrj1012@sina.com>
2018-05-31 19:20:05 +0800

    learn python oop

--- OOP/Student.py
+++ OOP/Student.py
@@ -1,21 +1,46 @@
 # utf-8
 
+# class Student(object):
+#     def __init__(self, name):
+#         self.__name = name
 
+#     def get_name(self):
+#         return self.__name
+
+#     def set_name(self, name):
+#         self.__name = name
+
+#     def call(self):
+#         print(self.__name)
+
+# if (__name__ == '__main__'):
+#     qiang = Student('qiang')
+#     # print(qiang._Student__name) #不同版本显示不同
+#     print(qiang.get_name())
+#     # print(qiang.age)
+#     chuang = Student('chuang')
+#     # print(chuang.age)
+#     qiang.call()
+#     chuang.call()
+
+
+#对property的理解
 class Student(object):
-    def __init__(self, name):
-        self.name = name
+    @property
+    def birth(self):
+        return self._birth
 
-    def call(self):
-        print(self.name)
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2018 - self._birth
 
 
-
-
-if(__name__=='__main__'):
-    qiang=Student('qiang')
-    qiang.age=29
-    print(qiang.age)
-    chuang=Student('chuang')
-    # print(chuang.age)
-    qiang.call()
-    chuang.call()
+if (__name__ == '__main__'):
+    s1 = Student()
+    s1.birth = 1990
+    # s1.age = 11
+    print(s1.age)
