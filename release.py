e8b846226c1b9f43e0b072f1da4b9eca1e8869d2
qianggezale <lqrj1012@sina.com>
2018-06-04 09:04:17 +0800

    learn os

--- release.py
+++ release.py
@@ -2,15 +2,20 @@
 
 from datetime import datetime
 import os
+import sys
+
 kuozhanming = ('.txt')
-myspantime = 20  #分钟
+myspantime = 20  #默认20分钟
 
 
 def deleteOutOfDateFile(abspath):
     for f in os.listdir(abspath):
         f_join = os.path.join(abspath, f)
         if (os.path.isdir(f_join)):
-            deleteOutOfDateFile(f_join)
+            if (not os.listdir(f_join)):
+                os.rmdir(f_join)
+            else:
+                deleteOutOfDateFile(f_join)
         if (os.path.isfile(f_join)):
             koutext = os.path.splitext(f)[1]
             if (koutext and (koutext in kuozhanming)):
@@ -28,4 +33,11 @@ def deleteOutOfDateFile(abspath):
 
 
 if (__name__ == '__main__'):
-    deleteOutOfDateFile('.')
+    try:
+        if (len(sys.argv) == 2):
+            myspantime = int(sys.argv[1])
+            deleteOutOfDateFile('.')
+        else:
+            print('请输入一个时间参数(单位分钟)')
+    except Exception as e:
+        print('出现错误：' + e)
